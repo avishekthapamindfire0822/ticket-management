eab4fd4b97d9558c20399468e3b40dbb1ed83acb
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-18 14:38:44 +0530

    Server:Add constants file in shared module

---
+++ server/src/shared/constants.js
@@ -0,0 +1,9 @@
+const CONSTANTS = {
+  EMAIL_ID_IS_REQUIRED: 'Email-Id is required.',
+  PASSWORD_IS_REQUIRED: 'Password is required.',
+  FIRSTNAME_IS_REQUIRED: 'Firstname is required.',
+  LASTNAME_IS_REQUIRED: 'Lastname is required.',
+  EMAIL_ID_REGEXP: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/,
+};
+
+module.exports = CONSTANTS;
