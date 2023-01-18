aec8a1ca0b8bd8efc9dd13f572d8778c21c25acd
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-18 15:50:04 +0530

    Server:Add ticket product types for different products

---
+++ server/src/ticket/ticket.product-types.js
@@ -0,0 +1,9 @@
+const ticketProductTypes = {
+  MOBILE_APP: 'MOBILE_APP',
+  WEBSITE: 'WEBSITE',
+  SUBSCRIPTIONS: 'Subscriptions',
+  GENERAL: 'GENERAL',
+  OTHER: 'OTHER',
+};
+
+module.exports = ticketProductTypes;
