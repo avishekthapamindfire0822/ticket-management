ac443fd401a2133dba542bb1e4c39429a58822f4
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-19 03:18:06 +0530

    Server:Fix populate data of user submitted the ticket

--- server/src/user/user.service.js
+++ server/src/user/user.service.js
@@ -3,11 +3,16 @@ const userRoles = require('./user.roles');
 
 const getUserTickets = async (userId, role) => {
   if (role === userRoles.IT_STAFF) {
-    return Ticket.find({});
+    return Ticket.find({}).populate(
+      'submittedBy',
+      'firstName lastName emailId'
+    );
   }
   return Ticket.find({
     submittedBy: userId,
-  }).select('_id description createdAt updatedAt');
+  })
+    .populate('submittedBy')
+    .select('_id description createdAt updatedAt');
 };
 
 module.exports = {
