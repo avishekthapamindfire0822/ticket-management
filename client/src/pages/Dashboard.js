d93cf9bd1fef58b9b0643cded19d9faf2bb0d972
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-19 00:47:45 +0530

    Client:Add dashboard page

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavMenu from '../component//nav/NavMenu';
+
+const Dashboard = () => {
+  return (
+    <>
+      <header>
+        <NavMenu />
+      </header>
+    </>
+  );
+};
+
+export default Dashboard;
