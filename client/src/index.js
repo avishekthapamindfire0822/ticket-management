0a8cb4d46d1510baf65aaec5cc4679b7ed8e1c84
avishek.thapa <avishek.thapa@mindfiresolutions.com>
2023-01-19 11:09:54 +0530

    Client:Add router for pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthContextProvider from './context/AuthContextProvider';
+import Dashboard from './pages/Dashboard';
+import ManageTicket from './pages/ManageTicket';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
+  {
+    path: '/manage-ticket',
+    element: <ManageTicket />,
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <RouterProvider router={router} />
+    </AuthContextProvider>
   </React.StrictMode>
 );
