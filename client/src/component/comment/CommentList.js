88ae4781ff8860758d62a97ce1f86f1781d9db43
avishekthapamindfire <avishek.thapa@mindfiresolutions.com>
2023-01-20 22:26:04 +0530

    Client:Add padding bottom to comment list

--- client/src/component/comment/CommentList.js
+++ client/src/component/comment/CommentList.js
@@ -3,7 +3,7 @@ import styles from './Comment.module.css';
 import CommentListItem from './CommentListItem';
 const CommentList = ({ comments }) => {
   return (
-    <div className={styles['comments']}>
+    <div className={`${styles['comments']} pb-3`}>
       {comments.map((comment) => (
         <CommentListItem key={comment._id} {...comment} />
       ))}
